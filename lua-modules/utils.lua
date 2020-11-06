e89364c1169a7c31c835525d4bdd9cd6aa8d648d
prabu-ssb <62878049+prabu-ssb@users.noreply.github.com>
2020-11-06 12:54:50 +0100

    Mimir 134 platform ready (#14)
    * tag not getting picked up propery
    
    * fix typo image name
    
    * yaml yak yak
    
    * yaml yak yak
    
    * yaml yak yak
    
    * dockerfile path not recognized..
    
    * MIMIR-134 : BIP review comments, OWB access url different from archive link urls
    
    * remove ipv6 port spec
    
    * adding container resolution
    
    * use standard global resolver
    
    * MIMIR-134 : turn off ssl verification resty:http
    
    * svc.local address not working
    
    * put back svc.local url, must get it fixed
    
    * put back svc.local url
    
    * replace url when owb redirects
    
    * diag, escape url, osv

--- lua-modules/utils.lua
+++ lua-modules/utils.lua
@@ -14,4 +14,21 @@ function _M.ensureNotNil(e)
     end
 end
 
+function _M.escapePattern (str)
+    return string.gsub(str, "([^%w])", "%%%1")
+end
+
+function _M.testEscape()
+    local orig = 'https://openwayback.staging-bip-app.ssb.no/wayback/20201027141355/https://www.qa.ssb.no/';
+    local pat = 'https://openwayback.staging-bip-app.ssb.no';
+    local rep = 'http://openwayback-ar.ssbno.svc.cluster.local';
+
+    ngx.log(ngx.DEBUG, pat);
+
+    local res = string.gsub(orig, _M.escapePattern(pat), rep);
+
+    ngx.log(ngx.DEBUG, orig .. ' => ' .. res);
+    return pat .. ' / ' .. res;
+end
+
 return _M;
